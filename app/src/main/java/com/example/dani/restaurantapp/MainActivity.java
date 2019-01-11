a8202b3236c5e393947ac5b3ec8adaf80d8b282f
Dani Pando Moreno <34524302+RedWolf3121@users.noreply.github.com>
2019-01-11 13:34:47 +0100

    .

--- app/src/main/java/com/example/dani/restaurantapp/MainActivity.java
+++ app/src/main/java/com/example/dani/restaurantapp/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        
+
        findViewById(R.id.registro).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
